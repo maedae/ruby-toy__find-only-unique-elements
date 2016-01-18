ae7209e5348ab2115d5181a61746b5e2d76fc29f
maedae <meagan.jungman@gmail.com>
2016-01-18 04:22:26 +0000

    push

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,20 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
+    arr_hash = Hash.new(0)
+    unique_arr = []
+
+    if arr.empty? 
+    return nil
+    end
+    arr.each do |arr| arr_hash[arr] += 1 
+    end
+    arr_hash.each do |key, value|
+        if value == 1
+            unique_arr.push(key)
+        end
+    end
   
+    return unique_arr
+	    
 end
